5f4be3a1c5dadb46f5021d632d9dbdfe5ad254ba
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-07-22 06:11:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,11 @@ describe "Scraper" do
       expect(doc).to be_a(Nokogiri::HTML::Document)
     end
   end
-
+  courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+   
+  courses.each do |course|
+    puts course.text.strip
+  end
   before(:each) do
     Course.reset_all
   end
